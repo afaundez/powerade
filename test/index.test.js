4940b4e2af015a1f9f7a7e67fec87e075682626d
Alvaro Faundez <alvaro@faundez.net>
2018-06-20 21:22:05 -0400

    Test with Jest (and babel)
    Testing init call return powerade instance

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+const Powerade = require('../src/index');
+let target = document.createElement('div')
+
+test('executes Powerade.init', () => {
+  let powerade = Powerade.init(target, [], { border: [ 'left' ], display: { label: false } })
+  expect(powerade.constructor.name)
+    .toBe('Powerade');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   output: {
     library: 'Powerade'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
   }
 };
