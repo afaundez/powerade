871e8f13d1524d14aef30ba70af00f36a66354c3
Alvaro Faundez <alvaro@faundez.net>
2018-08-03 15:12:09 -0400

    Refactor style, leving all in b/w and gray

--- src/example.js
+++ src/example.js
@@ -7,7 +7,7 @@ const outsideElements = [
 ];
 const insideElements = [...Array(6).keys()].map( position => {
   return {
-    label: `element ${position}`,
+    label: `element ${position**position**position}`,
     values: {
       'x-dimension': position,
       'y-dimension': position,
