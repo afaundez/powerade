d6c48c14a96adee2a47adc44938b39ceb0040eab
Alvaro Faundez <alvaro@faundez.net>
2018-09-22 12:49:43 -0400

    test draggable load

--- test/main.test.js
+++ test/main.test.js
@@ -1,17 +1,25 @@
 /* globals expect */
 const Powerade = require('../src/main');
 const target = document.createElement('div');
-const powerade = Powerade.init(target, [], {});
+import { elements, options }  from './fixtures/data/got.js';
+const powerade = Powerade.init(target, elements, options);
 
 test('executes Powerade.init succesfully', () => {
   expect(powerade.constructor.name).toBe('Powerade');
 });
 
 test('creates visualization structure', () => {
-  const dropzones = target.querySelectorAll('[data-drop-target]');
-  expect(dropzones.length).toBe(36);
-  const axes = target.querySelectorAll('[class*="header-"]');
+  const visualization = target.querySelector('.visualization');
+  const dropzones = visualization.querySelectorAll('[data-drop-target]');
+  expect(dropzones.length).toBe(25);
+  const axes = visualization.querySelectorAll('[class*="header-"]');
   expect(axes.length).toBe(3);
-  const legend = target.querySelectorAll('[class*="legend-"]');
+  const legend = visualization.querySelectorAll('[class*="legend-"]');
   expect(legend.length).toBe(1);
+});
+
+test('load elements into visualization', () => {
+  const visualization = target.querySelector('.visualization');
+  const draggables = visualization.querySelectorAll('[draggable="true"]');
+  expect(draggables.length).toBe(24);
 });
